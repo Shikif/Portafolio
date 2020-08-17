683eb132eca25815fb6b632874d8345bb0385c78
Jose Pablo Nordenflycht <diabhlo@gmail.com>
2020-08-17 03:39:19 -0400

    navbar smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 var navbar =    '<nav class="navbar navbar-expand-lg navbar-dark bg-primary">'+
-                    '<a class="navbar-brand" href="#">JPN</a>'+
+                    '<a class="navbar-brand" href="index.html">JPN</a>'+
                     '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">'+
                     '<span class="navbar-toggler-icon"></span>'+
                     '</button>'+
                     '<div class="collapse navbar-collapse" id="navbarNav">'+
                     '<ul class="navbar-nav ml-auto">'+
                         '<li class="nav-item active">'+
-                        '<a class="nav-link" href="#">Sobre mi <span class="sr-only">(current)</span></a>'+
+                        '<a class="nav-link" href="#About">Sobre mi <span class="sr-only">(current)</span></a>'+
                         '</li>'+
                         '<li class="nav-item">'+
-                        '<a class="nav-link" href="#">Experiencia</a>'+
+                        '<a class="nav-link" href="#Experience">Experiencia</a>'+
                         '</li>'+
                         '<li class="nav-item">'+
-                        '<a class="nav-link" href="#">Trabajos</a>'+
+                        '<a class="nav-link" href="#Jobs">Trabajos</a>'+
                         '</li>'+
                         '<li class="nav-item">'+
-                        '<a class="nav-link " href="#">Contacto</a>'+
+                        '<a class="nav-link " href="#Contact">Contacto</a>'+
                         '</li>'+
                     '</ul>'+
                     '</div>'+
