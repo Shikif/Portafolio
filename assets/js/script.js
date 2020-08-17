eb504f81a3a9aecb1dd319d65c1a2819587d8106
Jose Pablo Nordenflycht <diabhlo@gmail.com>
2020-08-17 12:06:43 -0400

    header listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,4 @@
-var navbar =    '<nav class="navbar navbar-expand-lg navbar-dark bg-primary">'+
-                    '<a class="navbar-brand" href="index.html">JPN</a>'+
-                    '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">'+
-                    '<span class="navbar-toggler-icon"></span>'+
-                    '</button>'+
-                    '<div class="collapse navbar-collapse" id="navbarNav">'+
-                    '<ul class="navbar-nav ml-auto">'+
-                        '<li class="nav-item active">'+
-                        '<a class="nav-link" href="#About">Sobre mi <span class="sr-only">(current)</span></a>'+
-                        '</li>'+
-                        '<li class="nav-item">'+
-                        '<a class="nav-link" href="#Experience">Experiencia</a>'+
-                        '</li>'+
-                        '<li class="nav-item">'+
-                        '<a class="nav-link" href="#Jobs">Trabajos</a>'+
-                        '</li>'+
-                        '<li class="nav-item">'+
-                        '<a class="nav-link " href="#Contact">Contacto</a>'+
-                        '</li>'+
-                    '</ul>'+
-                    '</div>'+
-                '</nav>'
+
 
 $('body').css('margin', '0');
 $('#navbar').append(navbar);
